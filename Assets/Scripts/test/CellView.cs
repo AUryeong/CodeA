6edf63c2b3ab4e0a134addcdbf9d6bde836bcd9a
AUryeong <jykee7030@gmail.com>
2023-05-03 20:39:51 +0900

    import recycable scroll, test scroll

---
+++ Assets/Scripts/test/CellView.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using EnhancedUI.EnhancedScroller;
+using TMPro;
+
+namespace EnhancedScrollerDemos.Chat
+{
+    /// <summary>
+    /// Super simple data class to hold information for each cell.
+    /// </summary>
+    public class Data
+    {
+        /// <summary>
+        /// The potential types of a cell
+        /// </summary>
+        public enum CellType
+        {
+            Spacer,
+            MyText,
+            OtherText
+        }
+
+        /// <summary>
+        /// The type of the cell
+        /// </summary>
+        public CellType cellType;
+
+        /// <summary>
+        /// The text to display (only used on chat cells, not the spacer)
+        /// </summary>
+        public string someText;
+
+        /// <summary>
+        /// We will store the cell size in the model so that the cell view can update it.
+        /// Only used on chat cells, not the spacer. Spacer always pulls the size of the scroll rect in the controller.
+        /// </summary>
+        public float cellSize;
+    }
+    /// <summary>
+    /// This is the view of our cell which handles how the cell looks.
+    /// </summary>
+    public class CellView : EnhancedScrollerCellView
+    {
+        /// <summary>
+        /// A reference to the UI Text element to display the cell data
+        /// </summary>
+        public TextMeshProUGUI someTextText;
+
+        /// <summary>
+        /// A reference to the rect transform which will be
+        /// updated by the content size fitter
+        /// </summary>
+        public RectTransform textRectTransform;
+
+        /// <summary>
+        /// The space around the text label so that we
+        /// aren't up against the edges of the cell
+        /// </summary>
+        public RectOffset textBuffer;
+
+        public void SetData(Data data)
+        {
+            someTextText.text = data.someText;
+            Canvas.ForceUpdateCanvases();         
+            data.cellSize = someTextText.textInfo.lineCount * someTextText.fontSize +20;
+        }
+    }
+}
